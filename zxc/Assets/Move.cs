e8ebf09049c79edfa551de8cf9756f7158fe9eba
ShutzPatron <76173389+ShutzPatron@users.noreply.github.com>
2023-06-10 20:02:58 +0300

    GG 1

--- zxc/Assets/Move.cs
+++ zxc/Assets/Move.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector3 moveVector;
+    public float turnInput;
+    public float mainSpeed = 10f;
+
+    void FixedUpdate()
     {
-        
+        Moving();   
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Moving()
     {
-        
+        Vector3 p = GetBaseInput();
+        if (p.sqrMagnitude > 0)
+        {
+            p = p * Time.deltaTime * mainSpeed;
+            transform.Translate(p);
+            Vector3 newPosition = transform.position;
+        }
+    }
+    private Vector3 GetBaseInput()
+    {
+        Vector3 p_Velocity = new Vector3();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation *= Quaternion.Euler(0, 5f * Time.deltaTime * mainSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation *= Quaternion.Euler(0, -5f * Time.deltaTime * mainSpeed, 0);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            p_Velocity += new Vector3(0, 1 * Input.GetAxis("Mouse ScrollWheel") * 10, 0);
+        }
+        return p_Velocity;
     }
 }
